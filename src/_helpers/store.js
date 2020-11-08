8c20259b962206a88afd3cc9761fe29f1acdee32
Thiago André <thiagoaas1990@gmail.com>
2020-11-08 12:53:39 -0300

    installed redux devtools

--- src/_helpers/store.js
+++ src/_helpers/store.js
@@ -1,14 +1,19 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import thunkMiddleware from 'redux-thunk'
 import { createLogger } from 'redux-logger'
 import rootReducer from '../_reducers'
 
 const loggerMiddleware = createLogger()
 
-export const store = createStore(
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(
     rootReducer,
-    applyMiddleware(
-        thunkMiddleware,
-        loggerMiddleware
+    composeEnhancers(
+      applyMiddleware(
+          thunkMiddleware,
+          loggerMiddleware
+      )
     )
 )
+
+export { store }
