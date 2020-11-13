e6d4de345919aef29d45daddb32302ede9119065
Luiza Amanajas <luizamanajas@hotmail.com>
2020-11-13 15:04:12 -0300

    Fix tela resumo da missão reseta estado das salas

--- src/pages/game1/Game/index.js
+++ src/pages/game1/Game/index.js
@@ -81,25 +81,25 @@ const Game = () => {
 			<div style={{width: '100%', height: '100%'}}>
 				<div id="RoomName">{rooms[state.currentRoom].nome}</div>
 					{ !state.gameEndState ? // Não dá para ser !endGame pq ele vira true na hora que aparece para o jogador se apresentar
-						missionOpen ? <Init onClose={ () => setMissionOpen(false) }/>
-							:	<div>
-									<button onClick={ () => setMissionOpen(true) }>Abrir resumo da missão</button>
-									<RoomSelect
-										roomsData={rooms}
-										onChange={(num) => {
-											setState({...state, currentRoom: num}) 
-										}}
-									/>
-									<Sala roomData={rooms[state.currentRoom]} setCurrentChar={setCurrentChar}/>
-									{ state.currentChar && !state.gameEndState ?
+						:	<div>
+								{missionOpen && <Init onClose={ () => setMissionOpen(false) }/>}
+								<button onClick={ () => setMissionOpen(true) }>Abrir resumo da missão</button>
+								<RoomSelect
+									roomsData={rooms}
+									onChange={(num) => {
+										setState({...state, currentRoom: num}) 
+									}}
+								/>
+								<Sala roomData={rooms[state.currentRoom]} setCurrentChar={setCurrentChar}/>
+								{ state.currentChar && !state.gameEndState ?
 
-										<Conversa endGame={state.endGame}
-											handleSubmit={handleSubmit} quizOptions={quizOptions}
-											charData={state.currentChar} checkEnd={checkEnd} clearCurrentChar={clearCurrentChar}
-										/>
-										: null
-									}
-								</div>
+									<Conversa endGame={state.endGame}
+										handleSubmit={handleSubmit} quizOptions={quizOptions}
+										charData={state.currentChar} checkEnd={checkEnd} clearCurrentChar={clearCurrentChar}
+									/>
+									: null
+								}
+							</div>
 						:
 						null
 					}
