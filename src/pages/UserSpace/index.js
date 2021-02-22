a5cdda33e26d91a1224533dcb1536c896f36cd3e
Gabriel Barbosa <gabrielfbbarbosa@gmail.com>
2021-02-22 19:54:58 -0300

    Card de missões

--- src/pages/UserSpace/index.js
+++ src/pages/UserSpace/index.js
@@ -17,18 +17,6 @@ const UserSpace = () => {
   return(
     <div id="userspace">
       <p>Oi {user.username}!</p>
-      <div>
-        <p>Jogar jogo 1</p>
-        <div id="missoes">Missões:
-          {missions.loading ? <div>Loading...</div> :
-            missions && missions.items.map( (mission, index) =>
-            <div key={index}>
-              <Link to={`/game/${mission.id}`}>{mission.name}</Link>
-            </div>)
-          }
-        </div>
-      </div>
-
       <div id="area-criacao">
         Área de criação
         <div>
@@ -41,6 +29,26 @@ const UserSpace = () => {
 
         <div>
           <Link to='/questions'>Perguntas</Link>
+        </div>
+      </div>
+
+      <div id="jogos">
+        <p>Jogar jogo 1</p>
+        <p>Missões:</p>
+        <div id="missoes">
+          {missions.loading ? <div id="loading">Loading...</div> :
+            missions && missions.items.map( (mission, index) =>
+            <div key={index} class="missao">
+              <Link to={`/game/${mission.id}`}>
+                <div id="imagem"></div>
+                <div id="card-text-wrapper">
+                  <div id="nome-missao">{mission.name}</div>
+                  <div id="descricao">Descrição</div>
+                  <div id="recompensas">Recompensas</div>
+                </div>
+              </Link>
+            </div>)
+          }
         </div>
       </div>
 
