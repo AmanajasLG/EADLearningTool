09f4521a96dcc46b8b9583de2da43393a2fded84
thiagoa2s <thiagoaas1990@gmail.com>
2021-03-07 21:17:08 -0300

    legibilidade apenas

--- src/pages/Game2/components/DialogCharacter/index.js
+++ src/pages/Game2/components/DialogCharacter/index.js
@@ -3,10 +3,14 @@ import React from 'react'
 const DialogCharacter = ({character, face}) => {
   return(
     <div id='CharacterPortrait'>
-      {<img src={character.characterAssets.length > 0 ? character.characterAssets.find(asset =>  asset.bodyPart === 'upperBody'
-      ).image[0].url: ""} alt="portrait" />}
-      {<img src={character.characterAssets.length > 0 ? character.characterAssets.find(asset =>  asset.bodyPart === 'face' && asset.type === face
-      ).image[0].url : ""} alt="portrait" />}
+      {<img src={character.characterAssets.length > 0 ?
+        character.characterAssets
+        .find(asset =>  asset.bodyPart === 'upperBody')
+        .image[0].url: ""} alt="portrait" />}
+      {<img src={character.characterAssets.length > 0 ?
+        character.characterAssets
+        .find(asset =>  asset.bodyPart === 'face' && asset.type === face)
+        .image[0].url : ""} alt="portrait" />}
 </div>
   )
 }
