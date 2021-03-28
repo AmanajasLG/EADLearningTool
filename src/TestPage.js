95c31ef0c3a84328c03d9bc38114c3c303068944
Gabriel Barbosa <gabrielfbbarbosa@gmail.com>
2021-03-28 00:45:49 -0300

    Conserta bug do Writer

--- src/TestPage.js
+++ src/TestPage.js
@@ -2,6 +2,9 @@ import React from 'react'
 import Conversa from './pages/Game2/components/Conversa'
 
 const TestPage = () => {
+	const [state, setState] = React.useState( {show: true} )
+
+	if( state.show )
 	return(
 		<Conversa
 			prevDialogHistory = {[
@@ -13,10 +16,11 @@ const TestPage = () => {
 				{question: "Tudo sim. E com você?", answer: "Tudo ótimo."}
 			]}
 			afterWriter = { () => {console.log("afterWriter")} }
-			onExited = { (dialogHistory) => {console.log("onExited");console.log(dialogHistory)} }
+			onExited = { (dialogHistory) => {console.log("onExited");console.log(dialogHistory);setState(false)} }
 			onConvoChoiceMade = { (answer) => {console.log("onConvoChoiceMade");console.log(answer)} }
 		/>
 	)
+	else return null
 }
 
 export default TestPage
