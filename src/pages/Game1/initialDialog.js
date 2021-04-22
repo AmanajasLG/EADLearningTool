03007ebcc33ef4b3bf73f75ac72d7098eb5af204
AmanajasLG <luizamanajas@hotmail.com>
2021-04-21 21:27:00 -0300

    Initial dialog js

---
+++ src/pages/Game1/initialDialog.js
@@ -0,0 +1,25 @@
+const initialValue = [
+    {
+        question: 'Oi!',
+        answers: 'Oi! Tudo bem?',
+    },
+    {
+        question: 'Olá!',
+        answers: 'Oi! Como vai?',
+    },
+    {
+        question: 'E aí?',
+        answers: 'Opa, beleza?',
+    },
+    {
+        question: 'Até mais!',
+        answers: 'Já vai...?',
+    },
+    {
+        question: 'Tchau!',
+        answers: 'Tudo certo contigo...?',
+    },
+]
+
+
+export default initialValue
