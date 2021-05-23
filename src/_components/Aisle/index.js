b64ea7f3a1f9c0e1eddb5c619e71c61b9dffe02b
Gabriel Barbosa <gabrielfbbarbosa@gmail.com>
2021-05-22 23:11:10 -0300

    Começa ajuste dos preços na prateleira

--- src/_components/Aisle/index.js
+++ src/_components/Aisle/index.js
@@ -10,7 +10,7 @@ const Aisle = ({ products, addProduct, toPreviousAisle, toNextAisle }) => {
 		<div id={styles.aisle}>
 			<button className={styles.btnChangeAisle} onClick={toPreviousAisle}>{"❮"}</button>
 			<div id={styles.shelves}>
-				<img src={shelf} alt="" style={{width:"100%"}}/>
+				<img src={shelf} alt=""/>
 				<div className={styles.items}>
 					{products.map((product, index) => (
 						[<div className={styles.products} key={index*2}>
@@ -19,15 +19,13 @@ const Aisle = ({ products, addProduct, toPreviousAisle, toNextAisle }) => {
 									key={index}
 									onClick={addProduct(product.name)}
 									className={styles.tooltip}
-									style={{
-										backgroundImage: "url(" + product.shelfImage + ")"
-									}}
+									style={{backgroundImage: "url(" + product.shelfImage + ")"}}
 								>
 									<span className={styles.tooltiptext}>{product.tooltip}</span>
 								</div>
 							))}
 						</div>,
-						<span key={index*2+1}>{product.tag}</span>]
+						<span key={index*2+1} className={styles.shelfPrice}>{product.tag}</span>]
 					))}
 				</div>
 			</div>
