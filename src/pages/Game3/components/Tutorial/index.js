417895a1b229ee5fc249bed7c8c824947ff460d8
Gabriel Barbosa <gabrielfbbarbosa@gmail.com>
2021-05-23 15:34:10 -0300

    Faz prateleiras, setas e carrinho serem relativamente responsivos

--- src/pages/Game3/components/Tutorial/index.js
+++ src/pages/Game3/components/Tutorial/index.js
@@ -1,5 +1,6 @@
 import React from "react";
-import "./index.scss";
+import tutorialStyles from "./index.module.scss";
+import gameStyles from "../../index.module.scss";
 import DialogCharacter from "../../../../_components/DialogCharacter";
 import Aisle from "../../../../_components/Aisle";
 
@@ -74,19 +75,19 @@ const Tutorial = ({
   const tutorialText = [
     ["Clique em qualquer item para adicioná-lo ao carrinho.", "Click on any item to add it to your cart."],
     ["Clique em qualquer item do seu carrinho para retirá-lo.", "Click on any item from your cart to remove it."],
-    ["Use as setas para navegar pelas prateleiras.", "Use the arrows t move through shelves."],
+    ["Use as setas para navegar pelas prateleiras.", "Use the arrows to move through shelves."],
     ["Passe o mouse sobre o bloco de notas para ver a lista de compras.", "Hover the mouse over the notepad to see the shopping list."],
     ["Clique no ícone de dinheiro para finalizar sua compra.", "Click on the cash icon to checkout."]
   ].map( (line, index) => {return {ptbr: line[0], preferred: line[1]} } )
 
-  const blobPosition = (state.tutorialLine === 2 || state.tutorialLine === 3) ? "blob-right" : "blob-left";
+  const blobPosition = (state.tutorialLine === 2 || state.tutorialLine === 3) ? tutorialStyles.blobRight : tutorialStyles.blobLeft;
 
   return (
     <div id="room-itself" className="tutorial">
       {state.step === 0 && (
-        <div>
+        <div id={tutorialStyles.tutorialGrid}>
           <div
-            className={"tutorial-blob " + blobPosition}
+            className={tutorialStyles.tutorialBlob + " " + blobPosition}
           >
             {(state.tutorialLine >= 0 && state.tutorialLine <= 4) && (
               <div className={blobPosition}>
@@ -114,10 +115,10 @@ const Tutorial = ({
             toPreviousAisle={toPreviousAisleNextLine}
             toNextAisle={toNextAisleNextLine}
           />
-          <div className="cart">
-            <div className="cart-items">
+          <div className={gameStyles.cart}>
+            <div className={gameStyles.cartItems}>
               {shoppingCart.map((product, index) => (
-                <div className="cart-item" key={index}>
+                <div className={gameStyles.cartItem} key={index}>
                   <img
                     src={
                       ingredientsList.find(
@@ -126,7 +127,7 @@ const Tutorial = ({
                     }
                     alt=""
                     onClick={removeProductNextLine(index)}
-                    className="cart-item-img"
+                    className={gameStyles.cartItemImg}
                   />
                   <span>{product.count}</span>
                 </div>
@@ -135,7 +136,6 @@ const Tutorial = ({
             <img
               src={cart}
               alt=""
-              style={{ marginTop: -50, position: "relative" }}
             />
           </div>
 
@@ -144,10 +144,10 @@ const Tutorial = ({
               onClick={shopListNextLine}
               src={listIcon}
               alt=""
-              className="list-icon"
+              className={gameStyles.listIcon}
             />
           )}
-          {state.tutorialLine === 4 && (
+          {state.tutorialLine === 4 && ( // Dinheiro
             <img
               onClick={() =>
                 setState({
@@ -162,7 +162,7 @@ const Tutorial = ({
 
           {state.shopList && (
             <div
-              className="shop-list"
+              className={gameStyles.shopList}
               style={{
                 backgroundImage: "url(" + ingredientsListBg + ")",
                 backgroundRepeat: "no-repeat",
@@ -174,13 +174,13 @@ const Tutorial = ({
                   <img
                     src={listCheck}
                     alt=""
-                    className="shop-list-item-check"
+                    className={gameStyles.shopListItemCheck}
                   />
 
                   <img
                     src={ingredient.image}
                     alt=""
-                    className="shop-list-item-img"
+                    className={gameStyles.shopListItemImg}
                   />
                   {ingredient.description}
                 </div>
