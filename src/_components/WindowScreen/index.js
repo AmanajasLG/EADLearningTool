6606efff4de5d4d0156b38ad79e7859da18be73e
thiagoa2s <thiagoaas1990@gmail.com>
2021-07-29 14:13:32 -0300

    starting window frame

--- src/_components/WindowScreen/index.js
+++ src/_components/WindowScreen/index.js
@@ -1,8 +1,14 @@
 import React from 'react'
 
 const WindowScreen = ({children, style, ...props}) => {
+  const colors = ['#f9afa1', '#ffdea9', '#cbe7de']
   return(
     <div style={{backgroundColor: '#aaaaff', ...style}} {...props}>
+      <div style={{backgroundColor: '#bbbbbb', height: '4%', paddingLeft: '1%'}}>
+        {colors.map( (color, index) =>
+          <div key={index} style={{backgroundColor: color, margin: '0.5%', display: 'inline-block', height: '40%', width:'1%', borderRadius: '80%'}}></div>
+        )}
+      </div>
       {children}
     </div>
   )
