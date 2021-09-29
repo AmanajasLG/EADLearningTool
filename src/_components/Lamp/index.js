cda2311de66a9a9717ac2f68c597b5c8b91bfc42
AmanajasLG <luizamanajas@hotmail.com>
2021-09-29 11:18:20 -0300

    ADJ lamp hover

--- src/_components/Lamp/index.js
+++ src/_components/Lamp/index.js
@@ -3,13 +3,15 @@ import { lamp_apagada, lamp_acesa } from "../../img";
 import "./index.scss";
 
 const Lamp = ({ img, onClick, message, ...props }) => {
-  const icon = [lamp_apagada, lamp_acesa]
-  if(img){
-    if(img[0]) icon[0] = img[0]
-    icon[1] = img[1] ? img[1] : img[0]
+  const icon = [lamp_apagada, lamp_acesa];
+  if (img) {
+    if (img[0]) icon[0] = img[0];
+    icon[1] = img[1] ? img[1] : img[0];
   }
+
   return (
     <div id="acusar" onClick={onClick} {...props}>
+      <div></div>
       <span>{message}</span>
       <img id="lamp-apagada" src={icon[0]} alt=""></img>
       <img id="lamp-acesa" src={icon[1]} alt=""></img>
