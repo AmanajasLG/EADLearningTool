a88896affd318a9fdb5a777a59b4f039131fd5e7
Gabriel Barbosa <gabrielfbbarbosa@gmail.com>
2021-11-30 12:24:10 -0300

    Jogo 7: Conserta escolha de destino

--- src/_components/Cities/index.js
+++ src/_components/Cities/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Iniciar } from "../Button";
-import "./index.scss";
+import styles from "./index.module.scss";
+
 const Cities = ({ value, cities, onPlaceClick, onConfirm }) => {
   const [state, setState] = React.useState(value);
   const onClick = (index) => () => {
@@ -25,28 +26,13 @@ const Cities = ({ value, cities, onPlaceClick, onConfirm }) => {
         {cities &&
           cities.map((city, index) => (
             <div
-              className="cityCard"
+              className={styles["cityCard"]}
+              id={city === state ? styles["selected"] : null}
               key={index}
-              style={{
-                backgroundColor: city === state ? "#f9afa1" : null,
-                cursor: "pointer",
-                width: "30%",
-                margin: "1%",
-                borderRadius: "8%/12%",
-                overflow: "hidden",
-              }}
               onClick={onClick(city)}
             >
               <img src={city.image} alt="" />
-              <p
-                style={{
-                  display: "block",
-                  textAlign: "center",
-                  color: "white",
-                }}
-              >
-                {city.name}
-              </p>
+              <p>{city.name}</p>
             </div>
           ))}
       </div>
@@ -58,7 +44,7 @@ const Cities = ({ value, cities, onPlaceClick, onConfirm }) => {
             position: "absolute",
             right: "5%",
             bottom: "-2.5%",
-            fontSize: "0.8rem",
+            fontSize: "0.7em",
           }}
         ></Iniciar>
       )}
